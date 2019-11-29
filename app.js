6c9c123010ab3e30ebbcb94734db3e53bdc228eb
patifc <patifc@gmail.com>
2019-11-29 14:52:37 +0000

    version 1.5

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ router.get('/get/html', function(req, res) {
 
  
 
-server.listen(process.env.PORT || 3003, process.env.IP, function(){
+server.listen(process.env.PORT || 3000, process.env.IP, function(){
 var addr = server.address();
 console.log("Server is listening at", addr.address + ":" + addr.port)
 });
