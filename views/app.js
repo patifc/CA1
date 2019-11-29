2f58662068ac5f51496741f4b22cc9e9c01d74a1
patifc <patifc@gmail.com>
2019-11-29 14:22:44 +0000

    version 1.4

--- views/app.js
+++ views/app.js
@@ -9,7 +9,33 @@ var http = require('http'),
 var router = express();
 var server = http.createServer(router);
 
-router.use(express.static(path.join(__dirname, 'views')));
+router.use(express.static(path.resolve(__dirname, 'views')));
+
+ // POST request to add to JSON & XML files
+router.post('/post/json', function(req, res) {
+
+  // Function to read in a JSON file, add to it & convert to XML
+  function appendJSON(obj) {
+    console.log(obj);
+    // Function to read in XML file, convert it to JSON, add a new object and write back to XML file
+    xmlFileToJs('Mypuppylist.xml', function(err, result) {
+      if (err) throw (err);
+      result.puppy.section[obj.sec_n].entree.push({'name': obj.name, 'dog': obj.dog,  'description': obj.description,  'breed': obj.breed,  'age': obj.age,  'sex': obj.sex});
+      console.log(result);
+      jsToXmlFile('Mypuppylist.xml', result, function(err) {
+        if (err) console.log(err);
+      })
+    })
+  }
+
+  // Call appendJSON function and pass in body of the current POST request
+  appendJSON(req.body);
+
+  // Re-direct the browser back to the page, where the POST request came from
+  res.redirect('back');
+ 
+
+});
 
 router.use(express.urlencoded({extended: true}));
 router.use(express.json());
@@ -30,7 +56,7 @@ function jsToXmlFile(filename, obj, cb) {
   fs.writeFile(filepath, xml, cb);
 }
 router.get('/', function(req, res) {
-     const index = fs.readFileSync('index.html', 'utf-8');
+    // const index = fs.readFileSync('index.html', 'utf-8');
      res.end(index);
      
 
@@ -56,31 +82,7 @@ router.get('/get/html', function(req, res) {
     res.end(result.toString());
   });
 
-  // POST request to add to JSON & XML files
-router.post('/post/json', function(req, res) {
-
-  // Function to read in a JSON file, add to it & convert to XML
-  function appendJSON(obj) {
-    console.log(obj);
-    // Function to read in XML file, convert it to JSON, add a new object and write back to XML file
-    xmlFileToJs('Mypuppylist.xml', function(err, result) {
-      if (err) throw (err);
-      result.puppy.section[obj.sec_n].entree.push({'name': obj.name, 'dog': obj.dog,  'description': obj.description,  'breed': obj.breed,  'age': obj.age,  'sex': obj.sex});
-      console.log(result);
-      jsToXmlFile('Mypuppylist.xml', result, function(err) {
-        if (err) console.log(err);
-      })
-    })
-  }
-
-  // Call appendJSON function and pass in body of the current POST request
-  appendJSON(req.body);
-
-  // Re-direct the browser back to the page, where the POST request came from
-  res.redirect('back');
  
-
-});
 
 server.listen(process.env.PORT || 3003, process.env.IP, function(){
 var addr = server.address();
