7414d55d0b7e46ce54a6d947e77056f7787f3af7
patifc <patifc@gmail.com>
2019-12-04 17:03:01 +0000

    version 1.5

--- app.js
+++ app.js
@@ -12,30 +12,7 @@ var server = http.createServer(router);
 router.use(express.static(path.resolve(__dirname, 'views')));
 
  // POST request to add to JSON & XML files
-router.post('/post/json', function(req, res) {
 
-  // Function to read in a JSON file, add to it & convert to XML
-  function appendJSON(obj) {
-    console.log(obj);
-    // Function to read in XML file, convert it to JSON, add a new object and write back to XML file
-    xmlFileToJs('Mypuppylist.xml', function(err, result) {
-      if (err) throw (err);
-      result.puppy.section[obj.sec_n].entree.push({'name': obj.name, 'dog': obj.dog,  'description': obj.description,  'breed': obj.breed,  'age': obj.age,  'sex': obj.sex});
-      console.log(result);
-      jsToXmlFile('Mypuppylist.xml', result, function(err) {
-        if (err) console.log(err);
-      })
-    })
-  }
-
-  // Call appendJSON function and pass in body of the current POST request
-  appendJSON(req.body);
-
-  // Re-direct the browser back to the page, where the POST request came from
-  res.redirect('back');
- 
-
-});
 
 router.use(express.urlencoded({extended: true}));
 router.use(express.json());
@@ -61,7 +38,33 @@ router.get('/', function(req, res) {
      
 
 })
+ 
+router.post('/post/json', function(req, res) {
+    console.log(req.body);
+  // Function to read in a JSON file, add to it & convert to XML
+  function appendJSON(obj) {
+    console.log(obj);
+    // Function to read in XML file, convert it to JSON, add a new object and write back to XML file
+    xmlFileToJs('Mypuppylist.xml', function(err, result) {
+      if (err) throw (err);
+      console.log(obj.name);
+      result.puppyListing.puppy.push({'name': obj.name, 'dog': obj.dog,  'description': obj.description,  'breed': obj.breed,  'age': obj.age,  'sex': obj.sex});
+      console.log(result);
+      jsToXmlFile('Mypuppylist.xml', result, function(err) {
+        if (err) console.log(err);
+      })
+    })
+  }
 
+  // Call appendJSON function and pass in body of the current POST request
+
+  appendJSON(req.body);
+
+  // Re-direct the browser back to the page, where the POST request came from
+  res.redirect('back');
+ 
+
+});
   
 
 
